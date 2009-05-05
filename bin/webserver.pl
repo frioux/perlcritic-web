f7de474ca940506328369f3d3e45adde37322807
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2009-05-04 23:20:31 -0500

    cleanup

--- bin/webserver.pl
+++ bin/webserver.pl
@@ -7,16 +7,12 @@ use lib "$FindBin::Bin/../lib";
 use WebCritic::Controller;
 use Readonly;
 Readonly my $port => shift || 5053;
-Readonly my $dir => shift || q{.};
+Readonly my $dir  => shift || q{.};
 
 my $server = CGI::Application::Server->new($port);
-my $app = WebCritic::Controller->new(PARAMS => {
-   dir => $dir
-});
+my $app = WebCritic::Controller->new(PARAMS => { dir => $dir });
 
-$server->entry_points({
-      '/critic'     => $app
-   });
+$server->entry_points({ '/critic' => $app });
 $server->document_root("$FindBin::Bin/../static");
 $server->run();
 
