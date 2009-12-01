12ca948a42822dd92568438dd00964aa527d1728
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2009-11-30 22:01:51 -0600

    CSS::Tags is almost CSS::Declare

--- lib/CSS/Tags.pm
+++ lib/CSS/Tags.pm
@@ -5,6 +5,74 @@ use warnings;
 
 use Perl6::Gather;
 
+
+#sub import {
+   #my $pkg = shift;
+   #my $callpkg = caller($ExportLevel);
+
+   #for my $property (@properties) {
+      #*{"$callpkg\::$property"} = sub { return ($property => $_[0]) };
+   #}
+#}
+
+my $IN_SCOPE = 0;
+
+sub import {
+  die "Can't import XML::Tags into a scope when already compiling one that uses it"
+    if $IN_SCOPE;
+  my ($class, @args) = @_;
+  my $opts = shift(@args) if ref($args[0]) eq 'HASH';
+  my $target = $class->_find_target(0, $opts);
+  my $unex = $class->_export_tags_into($target);
+  $class->_install_unexporter($unex);
+  $IN_SCOPE = 1;
+}
+
+sub _find_target {
+  my ($class, $extra_levels, $opts) = @_;
+  return $opts->{into} if defined($opts->{into});
+  my $level = ($opts->{into_level} || 1) + $extra_levels;
+  return (caller($level))[0];
+}
+
+my @properties = qw{
+background
+background_color
+border
+border_collapse
+border_top
+color
+float
+font_family
+font_size
+list_style_type
+margin
+padding
+};
+
+sub _export_tags_into {
+  my ($class, $into) = @_;
+   for my $property (@properties) {
+      my $property_name = $property;
+      $property_name =~ tr/_/-/;
+      no strict 'refs';
+      *{"$into\::$property"} = sub ($) { return ($property_name => $_[0]) };
+   }
+  return sub {
+    foreach my $property (@properties) {
+      no strict 'refs';
+      delete ${"${into}::"}{$property}
+    }
+    $IN_SCOPE = 0;
+  };
+}
+
+sub _install_unexporter {
+  my ($class, $unex) = @_;
+  $^H |= 0x120000; # localize %^H
+  $^H{'CSS::Tags::Unex'} = bless($unex, 'CSS::Tags::Unex');
+}
+
 sub to_css_string {
    my $css = shift;
 
@@ -24,5 +92,9 @@ sub _generate_declarations {
       }
    }).'}';
 }
+
+package CSS::Tags::Unex;
+
+sub DESTROY { local $@; eval { $_[0]->(); 1 } || warn "ARGH: $@" }
 
 1;
